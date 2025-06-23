3e72682fb9208c2290b9e8f6144ecea5c25aae69
Luis Henrique <luishenrique.gbarbosa21@gmail.com>
2025-06-23 02:46:40 -0300

    fix: suport cli for linux

--- cli.py
+++ cli.py
@@ -1,19 +1,30 @@
 from tabulate import tabulate
 import os
 import time
-import keyboard
+import curses
 
+# Cores ANSI para terminal normal
+ansi_colors = {
+    'reset': '\033[0m',
+    'red': '\033[91m',
+    'green': '\033[92m',
+    'yellow': '\033[93m',
+    'blue': '\033[94m',
+    'magenta': '\033[95m',
+    'cyan': '\033[96m',
+    'white': '\033[97m',
+}
 
-colors = {
-        'red': '\033[91m',
-        'green': '\033[92m',
-        'yellow': '\033[93m',
-        'blue': '\033[94m',
-        'magenta': '\033[95m',
-        'cyan': '\033[96m',
-        'white': '\033[97m',
-        'reset': '\033[0m'
-    }
+# Cores para curses
+curses_colors = {
+    'red': curses.COLOR_RED,
+    'green': curses.COLOR_GREEN,
+    'yellow': curses.COLOR_YELLOW,
+    'blue': curses.COLOR_BLUE,
+    'magenta': curses.COLOR_MAGENTA,
+    'cyan': curses.COLOR_CYAN,
+    'white': curses.COLOR_WHITE,
+}
 
 def clear_screen():
     if os.name == 'nt':
@@ -22,48 +33,79 @@ def clear_screen():
         os.system('clear')
 
 def print_colored_line(text, color='white'):
-    color_code = colors.get(color, colors['reset'])
-    print(f"{color_code}{text}{colors['reset']}")
+    color_code = ansi_colors.get(color, ansi_colors['reset'])
+    print(f"{color_code}{text}{ansi_colors['reset']}")
 
 def colored_string(text, color='white'):
-    color_code = colors.get(color, colors['reset'])
-    return f"{color_code}{text}{colors['reset']}"
+    color_code = ansi_colors.get(color, ansi_colors['reset'])
+    return f"{color_code}{text}{ansi_colors['reset']}"
 
 def select_opition(Title, options, index=0):
+    import curses
+
     if isinstance(options, dict):
         keys = list(options.keys())
     else:
         keys = options
 
-    while True:
-        clear_screen()
-        print(Title)
-        for i, key in enumerate(keys):
-            if i == index:
-                print_colored_line(key, 'cyan')
-            else:
-                print(key)
-
-        event = keyboard.read_event()
-        if event.event_type == keyboard.KEY_DOWN:
-            if event.name == 'down':
+    def menu(stdscr):
+        nonlocal index
+        curses.curs_set(0)
+        while True:
+            stdscr.clear()
+            stdscr.addstr(0, 0, Title + "\n")
+            for i, key in enumerate(keys):
+                if i == index:
+                    stdscr.addstr(i + 1, 0, f"> {key}\n", curses.color_pair(1))
+                else:
+                    stdscr.addstr(i + 1, 0, f"  {key}\n")
+            stdscr.refresh()
+            key_event = stdscr.getch()
+            if key_event == curses.KEY_DOWN:
                 index = (index + 1) % len(keys)
-            elif event.name == 'up':
+            elif key_event == curses.KEY_UP:
                 index = (index - 1) % len(keys)
-            elif event.name == 'enter':
+            elif key_event in [curses.KEY_ENTER, 10, 13]:
                 if isinstance(options, dict):
                     return options[keys[index]]
                 else:
                     return keys[index]
-            elif event.name == 'esc':
+            elif key_event == 27:  # ESC
                 return "Exit"
 
+    def wrapper(stdscr):
+        curses.start_color()
+        try:
+            curses.use_default_colors()
+            curses.init_pair(1, curses.COLOR_CYAN, -1)
+        except curses.error:
+            curses.init_pair(1, curses.COLOR_CYAN, curses.COLOR_BLACK)
+        return menu(stdscr)
+
+    return curses.wrapper(wrapper)
+
 def instruction(description, color='green'):
-    print_colored_line(description, color)
-    while True:
-        event = keyboard.read_event()
-        if event.event_type == keyboard.KEY_DOWN and event.name == 'enter':
-            break
+    def wait_enter(stdscr):
+        stdscr.clear()
+        curses.use_default_colors()
+        color_map = {
+            'red': curses.COLOR_RED,
+            'green': curses.COLOR_GREEN,
+            'yellow': curses.COLOR_YELLOW,
+            'blue': curses.COLOR_BLUE,
+            'magenta': curses.COLOR_MAGENTA,
+            'cyan': curses.COLOR_CYAN,
+            'white': curses.COLOR_WHITE,
+        }
+        curses.init_pair(2, color_map.get(color, curses.COLOR_WHITE),-1)
+        stdscr.addstr(0, 0, description + "\n", curses.color_pair(2))
+        stdscr.addstr(2, 0, "Pressione Enter para continuar...")
+        stdscr.refresh()
+        while True:
+            key = stdscr.getch()
+            if key in [curses.KEY_ENTER, 10, 13]:
+                break
+    curses.wrapper(wait_enter)
 
 def status_color(status):
     if status == 'success':
